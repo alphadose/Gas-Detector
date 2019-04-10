192758d57a9114e66ce1c56fd73caa2d401e53d1
Alphadose <anish.mukherjee1996@gmail.com>
2019-04-10 06:02:38 +0530

    add bluetooth data transmission as a background service

--- app/src/main/java/com/example/alphadose/gasdetector/MainActivity.java
+++ app/src/main/java/com/example/alphadose/gasdetector/MainActivity.java
@@ -39,65 +39,11 @@ public class MainActivity extends AppCompatActivity {
     private static List<Gas> gasList = new ArrayList<>();
     private static GasAdapter gasAdapter;
 
-    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-    public String DEVICE_ADDRESS = "30:58:01:D6:19:46";
-    public String DEVICE_NAME = "HC-05";
-    public UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
-    public BluetoothDevice device;
-    public Boolean found = false;
-    public BluetoothSocket socket;
-    public InputStream inputStream;
-
-    public Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            byte[] writeBuf = (byte[]) msg.obj;
-            int begin = (int)msg.arg1;
-            int end = (int)msg.arg2;
-
-            switch(msg.what) {
-                case 1:
-                    String data = new String(writeBuf);
-                    data = data.substring(begin, end);
-                    Log.d("Answer", data);
-                    updateGasData(data);
-                    break;
-            }
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-
-        if (bluetoothAdapter == null) {
-            Toast.makeText(getApplicationContext(),"Device doesnt Support Bluetooth",Toast.LENGTH_SHORT).show();
-        }
-        if(!bluetoothAdapter.isEnabled())
-        {
-            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableAdapter, 0);
-        }
-
-        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
-
-        if(bondedDevices.isEmpty()) {
-            Toast.makeText(getApplicationContext(),"Please Pair the Device first",Toast.LENGTH_SHORT).show();
-        } else {
-            for (BluetoothDevice iterator : bondedDevices) {
-                if(iterator.getName().startsWith("HC"))
-                {
-                    device = iterator;
-                    found = true;
-                    break;
-                }
-            }
-        }
-
-        ConnectThread mConnectThread = new ConnectThread(device);
-        mConnectThread.start();
 
         gasAdapter = new GasAdapter(gasList);
         final LinearLayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
@@ -107,94 +53,9 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(gasAdapter);
 
         prepareGasData();
-    }
 
-    private class ConnectThread extends Thread {
-
-        private final BluetoothSocket mmSocket;
-        private final BluetoothDevice mmDevice;
-
-        public ConnectThread(BluetoothDevice device) {
-            BluetoothSocket tmp = null;
-            mmDevice = device;
-            try {
-                tmp = device.createRfcommSocketToServiceRecord(PORT_UUID);
-            } catch (IOException e) { }
-            mmSocket = tmp;
-        }
-
-        public void run() {
-            try {
-                mmSocket.connect();
-            } catch (IOException connectException) {
-                try {
-                    mmSocket.close();
-                } catch (IOException closeException) { }
-                return;
-            }
-            ConnectedThread mConnectedThread = new ConnectedThread(mmSocket);
-            mConnectedThread.start();
-        }
-
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) { }
-        }
-    }
-
-    private class ConnectedThread extends Thread {
-
-        private final BluetoothSocket mmSocket;
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-
-        public ConnectedThread(BluetoothSocket socket) {
-            mmSocket = socket;
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-            try {
-                tmpIn = socket.getInputStream();
-                tmpOut = socket.getOutputStream();
-            } catch (IOException e) { }
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
-        }
-
-        public void run() {
-            byte[] buffer = new byte[1024];
-            int begin = 0;
-            int bytes = 0;
-            while (true) {
-                try {
-                    bytes += mmInStream.read(buffer, bytes, buffer.length - bytes);
-                    for(int i = begin; i < bytes; i++) {
-                        if(buffer[i] == ";".getBytes()[0]) {
-                            mHandler.obtainMessage(1, begin, i, buffer).sendToTarget();
-                            begin = i + 1;
-                            if(i == bytes - 1) {
-                                bytes = 0;
-                                begin = 0;
-                            }
-                        }
-                    }
-                } catch (IOException e) {
-                    break;
-                }
-            }
-        }
-
-        public void write(byte[] bytes) {
-            try {
-                mmOutStream.write(bytes);
-            } catch (IOException e) { }
-        }
-
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) { }
-        }
+        Intent intent = new Intent(this, BluetoothService.class);
+        startService(intent);
     }
 
     private void prepareGasData() {
